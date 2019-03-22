385f3d3316918a36a7ac3bd7948c9cbaca0909be
Reno Sorensen <renojames19@gmail.com>
2019-03-22 00:28:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -109,4 +109,16 @@ def self.first_student_in_grade_10
   end.first
 end
 
+def self.all_students_in_grade_9
+  sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE grade = 9
+  SQL
+
+  DB[:conn].execute(sql).map do |row|
+    self.new_from_db(row)
+  end
+end
+
 end

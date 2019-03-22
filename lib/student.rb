12ac8e8ef39d0109d52200d74c133cae43e040b4
Reno Sorensen <renojames19@gmail.com>
2019-03-22 00:29:11 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -116,7 +116,7 @@ def self.all_students_in_grade_X(num)
     WHERE grade = ?
   SQL
 
-  DB[:conn].execute(sql).map do |row|
+  DB[:conn].execute(sql, num).map do |row|
     self.new_from_db(row)
   end
 end
